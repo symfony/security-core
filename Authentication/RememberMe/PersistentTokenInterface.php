ccf59cd84de878e9c6c6550f7da0e2ac20b04fca
Fabien Potencier <fabien.potencier@gmail.com>
2013-08-07 17:06:33 +0200

    updated CHANGELOG for 2.0.24

---
+++ Authentication/RememberMe/PersistentTokenInterface.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Symfony\Component\Security\Core\Authentication\RememberMe;
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Interface to be implemented by persistent token classes (such as
+ * Doctrine entities representing a remember-me token)
+ *
+ * @author Johannes M. Schmitt <schmittjoh@gmail.com>
+ */
+interface PersistentTokenInterface
+{
+    /**
+     * Returns the class of the user
+     * @return string
+     */
+    public function getClass();
+
+    /**
+     * Returns the username
+     * @return string
+     */
+    public function getUsername();
+
+    /**
+     * Returns the series
+     * @return string
+     */
+    public function getSeries();
+
+    /**
+     * Returns the token value
+     * @return string
+     */
+    public function getTokenValue();
+
+    /**
+     * Returns the last time the cookie was used
+     * @return \DateTime
+     */
+    public function getLastUsed();
+}
